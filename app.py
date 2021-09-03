d5a66cc8262e2f640eafe84f15283b4b17cca0f3
codingpalette <codingpalette@gmail.com>
2021-09-03 15:52:53 +0900

    front 수정

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='./client/build', static_url_path='/')
+app = Flask(__name__, static_folder='./front/build', static_url_path='/')
 
 def hello():
     print('hello')
