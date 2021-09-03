6db5f54ab3b8bfea400e5eba75890d7b6f2818d3
codingpalette <codingpalette@gmail.com>
2021-09-03 15:42:04 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='./client/build', static_url_path='/')
+
+def hello():
+    print('hello')
+
+@app.route('/api')
+def api():
+    return 'api'
+
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000", debug=True)
